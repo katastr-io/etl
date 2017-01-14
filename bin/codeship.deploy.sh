50b177f0100050c575db7d70bb1696aeca44ca55
Michal Zimmermann <zimmicz@gmail.com>
2017-01-14 18:58:29 +0100

    m

--- bin/codeship.deploy.sh
+++ bin/codeship.deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 HOMEDIR="/opt/katastr.io"
+COMMIT=$(git log --oneline | head -n 1 | cut -d ' ' -f 1)
+PREV_COMMIT=$(git log --oneline | head -n 2 | cut -d ' ' -f 1 | tail -n 1)
 
 cd ~/clone
 rsync -aqz ./* codeship@193.85.199.37:${HOMEDIR}/etl-new && \
-ssh codeship@193.85.199.37 "rm -rf ${HOMEDIR}/etl/* && mv ${HOMEDIR}/etl-new/* ${HOMEDIR}/etl && rm -rf ${HOMEDIR}/etl-new"
+ssh codeship@193.85.199.37 "rm -rf ${HOMEDIR}/etl-${PREV_COMMIT}"
