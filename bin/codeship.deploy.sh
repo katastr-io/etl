700fe35cbe812582143de1d359f036a2d59c6384
Michal Zimmermann <zimmicz@gmail.com>
2017-01-14 18:59:23 +0100

    Add codeship deployment

--- bin/codeship.deploy.sh
+++ bin/codeship.deploy.sh
@@ -2,8 +2,10 @@
 
 HOMEDIR="/opt/katastr.io"
 COMMIT=$(git log --oneline | head -n 1 | cut -d ' ' -f 1)
-PREV_COMMIT=$(git log --oneline | head -n 2 | cut -d ' ' -f 1 | tail -n 1)
 
 cd ~/clone
-rsync -aqz ./* codeship@193.85.199.37:${HOMEDIR}/etl-new && \
-ssh codeship@193.85.199.37 "rm -rf ${HOMEDIR}/etl-${PREV_COMMIT}"
+sed -i "s/COMMIT/${COMMIT}/g" etc/crontab.txt
+
+rsync -aqz ./* codeship@193.85.199.37:${HOMEDIR}/etl-${COMMIT} && \
+ssh codeship@193.85.199.37 "find ${HOMEDIR} -maxdepth 1 -type d ! -name etl-${COMMIT} -name 'etl-*' -exec rm -rf '{}' \;" && \
+ssh codeship@193.85.199.37 "crontab ${HOMEDIR}/etl-${COMMIT}/etc/crontab.txt"
